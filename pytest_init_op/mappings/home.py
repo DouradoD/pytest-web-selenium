4d49008c8821f3cd2b244ad427dff57dccfac743
Diogo Dourado <douradofadiogo@gmail.com>
2025-02-25 17:10:25 -0300

    [Refactor] Update the locators from pytest_init_op home mapping

--- pytest_init_op/mappings/home.py
+++ pytest_init_op/mappings/home.py
@@ -3,5 +3,6 @@ from selenium.webdriver.common.by import By
 
 class HomeMappings:
     LOGO = (By.CSS_SELECTOR, '[id="navbar"] img[class="navbar-logo-img"]')
-    INPUT_SEARCH_FIELD = (By.CSS_SELECTOR, '[id="navbar"] input[id="search-input"]')
-    BTN_SEARCH = (By.CSS_SELECTOR, '[id="navbar"] button[class*="search"]')
+    INPUT_SEARCH_FIELD = (By.XPATH, '//input[@id="search-text"]')
+    BTN_SEARCH = (By.CSS_SELECTOR, 'button[id="search-lens-btn"]')
+    TXT_SEARCH_QUERY = (By.ID, 'search-query-label')
--- pytest_init_op/steps/test_web_search_steps.py
+++ pytest_init_op/steps/test_web_search_steps.py
@@ -3,20 +3,20 @@ from selenium.webdriver.support import expected_conditions as exp_conditions
 from pytest_init_op.pages.home import home_page
 
 scenarios('../features/search.feature')
-
+home = home_page()
 
 @given('the user is in the numerade home screen')
 def impl(driver):
     driver.get('https://www.numerade.com/')
-    driver.wait.until(exp_conditions.visibility_of_element_located(home_page().mapping.LOGO))
+    driver.wait.until(exp_conditions.visibility_of_element_located(home.mapping.LOGO))
 
 
 @given(parsers.parse('He fills the search filed with the following word "{word}"'))
 def impl(driver, word):
-    driver.wait.until(exp_conditions.visibility_of_element_located(home_page().mapping.INPUT_SEARCH_FIELD)).send_keys(
+    driver.wait.until(exp_conditions.visibility_of_element_located(home.mapping.INPUT_SEARCH_FIELD)).send_keys(
         word)
 
 
 @when('He clicks on search button')
 def impl(driver):
-    driver.wait.until(exp_conditions.visibility_of_element_located(home_page().mapping.BTN_SEARCH)).click()
+    driver.wait.until(exp_conditions.visibility_of_element_located(home.mapping.BTN_SEARCH)).click()
